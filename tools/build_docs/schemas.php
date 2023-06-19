2f28a7fb79ec52e351e82f3db7b2995f2d41a5f2
Jonaydban <119614215+Jonaydban@users.noreply.github.com>
2023-06-19 15:41:35 +0300

    Add files via upload

---
+++ tools/build_docs/schemas.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * Load schema file names.
+ *
+ * @return array
+ */
+function loadSchemas(): array
+{
+    $res = [];
+    foreach (\glob(\getcwd().'/schemas/TL_telegram_*') as $file) {
+        \preg_match("/telegram_v(\d+)/", $file, $matches);
+        $res[$matches[1]] = $file;
+    }
+    \ksort($res);
+    return $res;
+}
+
+/**
+ * Return max available layer number.
+ *
+ * @param array $schemas Scheme array
+ *
+ * @return integer
+ */
+function maxLayer(array $schemas): int
+{
+    $schemas = \array_keys($schemas);
+    return \end($schemas);
+}
+
+/**
+ * Init docs.
+ *
+ * @param array $layers Scheme array
+ *
+ * @return array Documentation information for old docs
+ */
+function initDocs(array $layers): array
+{
+    return [];
+}
