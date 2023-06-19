b9ef80a37d13986af7253bdb4a432af804b5c672
Jonaydban <119614215+Jonaydban@users.noreply.github.com>
2023-06-19 15:49:31 +0300

    Add files via upload

---
+++ src/docs/MadelineProto/Db/NullCache/NullCacheTrait.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace danog\MadelineProto\Db\NullCache;
+
+/**
+ * Trait that disables database caching.
+ *
+ * @internal
+ */
+trait NullCacheTrait
+{
+    protected function setCacheTtl(int $ttl): void
+    {
+    }
+    protected function hasCache(string $key): bool
+    {
+        return false;
+    }
+    /**
+     * @return null
+     */
+    protected function getCache(string $key)
+    {
+        throw new \RuntimeException('Not implemented!');
+    }
+
+    /**
+     * Save item in cache.
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    protected function setCache(string $key, $value): void
+    {
+    }
+
+    /**
+     * Remove key from cache.
+     *
+     * @param string $key
+     */
+    protected function unsetCache(string $key): void
+    {
+    }
+
+    protected function clearCache(): void
+    {
+    }
+
+    protected function startCacheCleanupLoop(): void
+    {
+    }
+    protected function stopCacheCleanupLoop(): void
+    {
+    }
+}
