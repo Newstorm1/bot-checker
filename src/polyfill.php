a45f675dd5659836d9570781880434260abc4973
Jonaydban <119614215+Jonaydban@users.noreply.github.com>
2023-06-19 15:28:17 +0300

    Add files via upload

---
+++ src/polyfill.php
@@ -0,0 +1,60 @@
+<?php
+
+if (PHP_OS_FAMILY === 'Windows') {
+    echo(PHP_EOL.'========='.PHP_EOL.'WARNING: MadelineProto does not support Windows, please use Linux or another UNIX system (WSL on Windows, Mac OS, BSD, etc).'.PHP_EOL.'========='.PHP_EOL.PHP_EOL);
+}
+
+// Polyfill for some PHP 5 functions
+function callMe($allable, ...$args)
+{
+    return $allable(...$args);
+}
+function returnMe($res)
+{
+    return $res;
+}
+function __coalesce($ifNotNull, $then)
+{
+    return $ifNotNull ?: $then;
+}
+function __destructure($list, $value): array
+{
+    $res = [];
+    foreach ($list as $key) {
+        if (\is_string($key)) {
+            $res[] = $value[$key];
+        } else {
+            $res = \array_merge($res, __destructure($key, $value[$key]));
+        }
+    }
+    return $res;
+}
+
+$ampFilePolyfill = 'namespace Amp\\File {';
+foreach ([
+    'open' => 'openFile',
+    'stat' => 'getStatus',
+    'lstat' => 'getLinkStatus',
+    'size' => 'getSize',
+    'isdir' => 'isDirectory',
+    'mtime' => 'getModificationTime',
+    'atime' => 'getAccessTime',
+    'ctime' => 'getCreationTime',
+    'symlink' => 'createSymlink',
+    'link' => 'createHardlink',
+    'readlink' => 'resolveSymlink',
+    'rename' => 'move',
+    'unlink' => 'deleteFile',
+    'rmdir' => 'deleteDirectory',
+    'scandir' => 'listFiles',
+    'chmod' => 'changePermissions',
+    'chown' => 'changeOwner',
+    'get' => 'read',
+    'put' => 'write',
+    'mkdir' => 'createDirectory',
+] as $old => $new) {
+    $ampFilePolyfill .= "function $old(...\$args) { return $new(...\$args); }";
+}
+$ampFilePolyfill .= "}";
+eval($ampFilePolyfill);
+unset($ampFilePolyfill);
